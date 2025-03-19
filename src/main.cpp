1c46935b7920acb1985296e019a14aeb7b99f00d
memr404 <memr404@gmail.com>
2025-03-19 22:04:32 +0300

    First Attempt to add the button
    v0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,40 @@
-/**
- * Include the Geode headers.
- */
 #include <Geode/Geode.hpp>
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
-/**
- * `$modify` lets you extend and modify GD's classes.
- * To hook a function in Geode, simply $modify the class
- * and write a new function definition with the signature of
- * the function you want to hook.
- *
- * Here we use the overloaded `$modify` macro to set our own class name,
- * so that we can use it for button callbacks.
- *
- * Notice the header being included, you *must* include the header for
- * the class you are modifying, or you will get a compile error.
- *
- * Another way you could do this is like this:
- *
- * struct MyMenuLayer : Modify<MyMenuLayer, MenuLayer> {};
- */
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	/**
-	 * Typically classes in GD are initialized using the `init` function, (though not always!),
-	 * so here we use it to add our own button to the bottom menu.
-	 *
-	 * Note that for all hooks, your signature has to *match exactly*,
-	 * `void init()` would not place a hook!
-	*/
-	bool init() {
-		/**
-		 * We call the original init function so that the
-		 * original class is properly initialized.
-		 */
-		if (!MenuLayer::init()) {
+#include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/binding/CCMenuItemSpriteExtra.hpp>
+#include <Geode/binding/PlayLayer.hpp>
+#include <Geode/cocos/sprite_nodes/CCSprite.h>
+
+class $modify(MyButtonInfoLayer, LevelInfoLayer) {
+	void onPracticeMode(CCObject* target) {
+        auto level = m_level;
+        if (level) {
+			log::info("TESTS", this->getChildrenCount());
+            //PlayLayer::togglePracticeMode(true); // Запуск уровня в режиме практики
+        }
+    }
+    bool init(GJGameLevel* level, bool p1){
+		if (!LevelInfoLayer::init(level, p1)) {
 			return false;
 		}
 
-		/**
-		 * You can use methods from the `geode::log` namespace to log messages to the console,
-		 * being useful for debugging and such. See this page for more info about logging:
-		 * https://docs.geode-sdk.org/tutorials/logging
-		*/
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+		auto menu = this->getChildByID("play-menu");
+        if (!menu) {
+			return true;
+		}
 
-		/**
-		 * See this page for more info about buttons
-		 * https://docs.geode-sdk.org/tutorials/buttons
-		*/
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			/**
-			 * Here we use the name we set earlier for our modify class.
-			*/
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+        auto practiceSprite = CCSprite::createWithSpriteFrameName("GJ_practiceBtn_001.png");
+        auto practiceBtn = CCMenuItemSpriteExtra::create(practiceSprite, this, menu_selector(MyButtonInfoLayer::onPracticeMode));
 
-		/**
-		 * Here we access the `bottom-menu` node by its ID, and add our button to it.
-		 * Node IDs are a Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/nodetree
-		*/
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		/**
-		 * The `_spr` string literal operator just prefixes the string with
-		 * your mod id followed by a slash. This is good practice for setting your own node ids.
-		*/
-		myButton->setID("my-button"_spr);
-
-		/**
-		 * We update the layout of the menu to ensure that our button is properly placed.
-		 * This is yet another Geode feature, see this page for more info about it:
-		 * https://docs.geode-sdk.org/tutorials/layouts
-		*/
-		menu->updateLayout();
-
-		/**
-		 * We return `true` to indicate that the class was properly initialized.
-		 */
-		return true;
-	}
-
-	/**
-	 * This is the callback function for the button we created earlier.
-	 * The signature for button callbacks must always be the same,
-	 * return type `void` and taking a `CCObject*`.
-	*/
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
+        auto playBtn = static_cast<CCMenuItemSpriteExtra*>(menu->getChildByID("play-button"));
+        if (playBtn) {
+			playBtn->setPosition(playBtn->getPosition() - ccp(20, 0));
+            practiceBtn->setPosition(playBtn->getPosition() + ccp(67, 0));
+			practiceBtn->setScale(0.7f); 
+        }
+		menu->addChild(practiceBtn);
+        return true;
 	}
 };
