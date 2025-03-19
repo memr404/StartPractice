61821299df7ccc2814697e4f30500b777ab6b15a
memr404 <memr404@gmail.com>
2025-03-19 23:49:02 +0300

    The functionality has been thoroughly improved
    v0.2.0
    
    Co-Authored-By: FineVBB724 <157292412+finevbb724@users.noreply.github.com>

--- changelog.md
+++ changelog.md
@@ -1,2 +1,5 @@
-# v0.1
+# v0.1.0
 - Added a button without functionality
+
+# v0.2.0
+- The functionality has been thoroughly improved
--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,12 @@ using namespace geode::prelude;
 
 #include <Geode/modify/LevelInfoLayer.hpp>
 #include <Geode/binding/CCMenuItemSpriteExtra.hpp>
-#include <Geode/binding/PlayLayer.hpp>
 #include <Geode/cocos/sprite_nodes/CCSprite.h>
+#include <Geode/modify/PlayLayer.hpp>
 
 class $modify(MyButtonInfoLayer, LevelInfoLayer) {
-	void onPracticeMode(CCObject* target) {
-        auto level = m_level;
-        if (level) {
-			log::info("TESTS", this->getChildrenCount());
-            //PlayLayer::togglePracticeMode(true); // Запуск уровня в режиме практики
-        }
-    }
+	static int practice;
+
     bool init(GJGameLevel* level, bool p1){
 		if (!LevelInfoLayer::init(level, p1)) {
 			return false;
@@ -24,9 +19,10 @@ class $modify(MyButtonInfoLayer, LevelInfoLayer) {
         if (!menu) {
 			return true;
 		}
+		practice = 0;
 
         auto practiceSprite = CCSprite::createWithSpriteFrameName("GJ_practiceBtn_001.png");
-        auto practiceBtn = CCMenuItemSpriteExtra::create(practiceSprite, this, menu_selector(MyButtonInfoLayer::onPracticeMode));
+        auto practiceBtn = CCMenuItemSpriteExtra::create(practiceSprite, this, menu_selector(MyButtonInfoLayer::togglePractice));
 
         auto playBtn = static_cast<CCMenuItemSpriteExtra*>(menu->getChildByID("play-button"));
         if (playBtn) {
@@ -37,4 +33,29 @@ class $modify(MyButtonInfoLayer, LevelInfoLayer) {
 		menu->addChild(practiceBtn);
         return true;
 	}
+
+	void togglePractice(CCObject* target) {
+		practice = 1;
+		this->onPlay(target);
+	}
+
+	static int getPractice() {
+		return practice;
+	}
 };
+
+class $modify (AutoPractice, PlayLayer)
+{
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects)
+    {
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects))
+            return false;
+
+		auto practice = MyButtonInfoLayer::getPractice();
+
+        this->togglePracticeMode(practice);
+        return true;
+    }
+};
+
+int MyButtonInfoLayer::practice = 0;
