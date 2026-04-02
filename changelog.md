59bcde1a840d81a317447d1f9a2ced4b4dd5fa9d
Руслан Рябинин <memr404@gmail.com>
2026-04-02 18:04:25 +0300

    Add button to EditLevelLayer

--- changelog.md
+++ changelog.md
@@ -1,13 +1,21 @@
+# v0.3.0
+- Add button to EditLevelLayer
+
 # v0.2.4
 - Not local build
+
 # v0.2.3
 - Update to Geode v5.4.1
+
 # v0.2.2
 - Rewrite everything
 - Crossplatform support
+
 # v0.2.1
 - Resizing the mod logo for Geode
+
 # v0.2.0
 - The functionality has been thoroughly improved
+
 # v0.1.0
 - Added a button without functionality
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Geode/modify/LevelInfoLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/EditLevelLayer.hpp>
 
 using namespace geode::prelude;
 
 bool practice = false;
 
-class $modify(MyButtonInfoLayer, LevelInfoLayer) {
+class $modify(PracticeButtonInfoLayer, LevelInfoLayer) {
 
     bool init(GJGameLevel* level, bool p1){
 		if (!LevelInfoLayer::init(level, p1)) return false;
@@ -13,11 +14,11 @@ class $modify(MyButtonInfoLayer, LevelInfoLayer) {
 		practice = false;
 
         auto practiceSprite = CCSprite::createWithSpriteFrameName("GJ_practiceBtn_001.png");
-        auto practiceBtn = CCMenuItemSpriteExtra::create(practiceSprite, this, menu_selector(MyButtonInfoLayer::togglePractice));
+        auto practiceBtn = CCMenuItemSpriteExtra::create(practiceSprite, this, menu_selector(PracticeButtonInfoLayer::togglePractice));
 		
 		auto playMenu = this->getChildByID("play-menu");
 		auto playBtn = static_cast<CCMenuItemSpriteExtra*>(this->querySelector("play-menu > play-button"));
-        if (!Mod::get()->getSettingValue<bool>("use-left-menu") && playBtn && playMenu) {
+        if (!Mod::get()->getSettingValue<bool>("use-menu-buttons") && playBtn && playMenu) {
         	playBtn->setPosition(playBtn->getPosition() - ccp(20, 0));
 			practiceBtn->setPosition(playBtn->getPosition() + ccp(67, 0));
 			practiceBtn->setScale(0.7f);
@@ -25,6 +26,38 @@ class $modify(MyButtonInfoLayer, LevelInfoLayer) {
 		} else if (auto leftMenu = this->getChildByID("left-side-menu")) {
 			leftMenu->addChild(practiceBtn);
 			leftMenu->updateLayout();
+		} else return true;
+
+		practiceBtn->setID("practice-mode-button"_spr);
+
+        return true;
+	}
+
+	void togglePractice(CCObject* target) {
+		this->onPlay(target);
+		practice = true;
+	}
+};
+
+
+class $modify(PracticeButtonEditLayer, EditLevelLayer) {
+
+    bool init(GJGameLevel* level){
+		if (!EditLevelLayer::init(level)) return false;
+
+		practice = false;
+
+        auto practiceSprite = CCSprite::createWithSpriteFrameName("GJ_practiceBtn_001.png");
+        auto practiceBtn = CCMenuItemSpriteExtra::create(practiceSprite, this, menu_selector(PracticeButtonEditLayer::togglePractice));
+		
+		auto playMenu = this->getChildByID("level-edit-menu");
+		auto playBtn = static_cast<CCMenuItemSpriteExtra*>(this->querySelector("level-edit-menu > play-button"));
+        if (!Mod::get()->getSettingValue<bool>("use-menu-buttons") && playBtn && playMenu) {
+			playMenu->addChild(practiceBtn);
+			playMenu->updateLayout();
+		} else if (auto Menu = this->getChildByID("level-actions-menu")) {
+			Menu->addChild(practiceBtn);
+			Menu->updateLayout();
 		} else return true;
 
 		practiceBtn->setID("practice-mode-button"_spr);
